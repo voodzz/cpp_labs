5a7154a5ffc54d52e8e3e0a7dc2ea97c699e4ee8
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2023-10-29 13:03:10 +0300

    commit

--- cppLabs/Lab_05_STL/task6/tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "cmake-build-debug/_deps/googletest-src/googletest/include/gtest/gtest.h"
-#include "../task6/functions.h"
-
-TEST(completeTasksTest, completeTasksPositive) {
-    std::vector<int> result = {1, 2, 2, 1, 2};
-    int k = 5;
-    std::vector<std::pair<int, int>> input = {{1, 4}, {2, 3}};
-    EXPECT_EQ(completeTasks(input, k), result);
-}
